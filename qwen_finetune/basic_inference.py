f3454d7a8c9b6e5b2eeb4abb3fdfe835d262237a
mathzxw2002 <mathzxw2002@gmail.com>
2025-04-22 22:39:23 +0800

    Update basic_inference.py

--- qwen_finetune/basic_inference.py
+++ qwen_finetune/basic_inference.py
@@ -1,5 +1,8 @@
 from vllm import LLM, SamplingParams
 
+#https://github.com/vllm-project/vllm/issues/6723https://github.com/vllm-project/vllm/issues/6723
+
+
 # Sample prompts.
 prompts = [
     "You will be given a problem. Please reason step by step, and put your final answer within \boxed{} : .3. (6 points) A construction company was building a tunnel. When $\frac{1}{3}$ of the tunnel was completed at the original speed, they started using new equipment, which increased the construction speed by $20 \%$ and reduced the working hours to $80 \%$ of the original. As a result, it took a total of 185 days to complete the tunnel. If they had not used the new equipment and continued at the original speed, it would have taken $\qquad$ days to complete the tunnel.",
